26ab05e191a81d5af07897859e3c08a989f9cb38
Your Name <you@example.com>
2025-07-31 16:46:30 +0700

    Refactor WordPuzzleGame UI: remove unnecessary SVG icons from buttons for cleaner interface

--- src/components/WordPuzzleGame.jsx
+++ src/components/WordPuzzleGame.jsx
@@ -424,18 +424,6 @@ export default function WordPuzzleGame({ userId }) {
           disabled={selected.length === 0 || timeLeft === 0}
         >
           <span className="flex items-center justify-center text-sm sm:text-base">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 sm:h-5 sm:w-5 mr-1"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
-                clipRule="evenodd"
-              />
-            </svg>
             🔄 Làm lại
           </span>
         </button>
@@ -444,18 +432,6 @@ export default function WordPuzzleGame({ userId }) {
           onClick={handleNext}
         >
           <span className="flex items-center justify-center text-sm sm:text-base">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 sm:h-5 sm:w-5 mr-1"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
             🎲 Từ khác
           </span>
         </button>
@@ -472,19 +448,6 @@ export default function WordPuzzleGame({ userId }) {
           disabled={showAnswer}
         >
           <span className="flex items-center justify-center text-sm sm:text-base">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 sm:h-5 sm:w-5 mr-1"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-              <path
-                fillRule="evenodd"
-                d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                clipRule="evenodd"
-              />
-            </svg>
             👀 Xem đáp án
           </span>
         </button>
